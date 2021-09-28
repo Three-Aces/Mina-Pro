aef843fd968ce624c1c7ae1d282468a139e24a2e
josephdagijimana <Emile-SHUMBUSHO>
2021-09-28 08:06:20 +0200

    changes on the project structure\

--- tombora/apps.py
+++ tombora/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WiningConfig(AppConfig):
+class TomboraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wining'
+    name = 'tombora'
